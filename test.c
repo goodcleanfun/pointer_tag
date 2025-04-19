133f15ff712efb4defd51a9ed3bd63441348f257
Al <albarrentine@gmail.com>
2025-04-19 16:33:01 -0400

    [test] adding tests and Github Actions

---
+++ test.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+
+#include "greatest/greatest.h"
+#include "aligned/aligned.h"
+
+#include "pointer_tag.h"
+
+#define TAG 0x1
+
+TEST test_pointer_tag(void) {
+    uint32_t *ptr = (uint32_t *)aligned_malloc(64, 64);
+    ASSERT_FALSE(pointer_is_tagged(ptr, TAG));
+    uint32_t *tagged_ptr = pointer_tag(ptr, TAG);
+    ASSERT_NEQ(ptr, tagged_ptr);
+    ASSERT(pointer_is_tagged(tagged_ptr, TAG));
+    uint32_t *untagged_ptr = pointer_untag(tagged_ptr, TAG);
+    ASSERT_EQ(untagged_ptr, ptr);
+    free(ptr);
+    PASS();
+}
+
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char **argv) {
+    GREATEST_MAIN_BEGIN();
+    RUN_TEST(test_pointer_tag);
+    GREATEST_MAIN_END();
+}
